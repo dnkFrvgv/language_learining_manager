080f72857055f551a1ff6bb6d4b220e32a9382de
Dominique <carinefrvga@gmail.com>
2023-07-06 12:01:40 -0300

    correcting repeated seed error

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -8,61 +8,66 @@ namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+      private static async Task SeedLanguages(DataContext context){
+        if (context.Languages.Any()) return;
+
+        var languages = new List<Language>
         {
-            if (!context.Languages.Any()) {
-
-            
-              var languages = new List<Language>
-              {
-                  new Language 
-                  {
-                    Title = "English",
-                  },
-                  new Language 
-                  {
-                    Title = "Spanish",
-                  },
-                  new Language
-                  {
-                    Title = "German",
-                  }
-              };
-
-              await context.Languages.AddRangeAsync(languages);
+            new Language 
+            {
+              Title = "English",
+            },
+            new Language 
+            {
+              Title = "Spanish",
+            },
+            new Language
+            {
+              Title = "German",
             }
+        };
 
-            if (!context.Skills.Any()) {
+        await context.Languages.AddRangeAsync(languages);
+        await context.SaveChangesAsync();
+      }
+      private static async Task SeedSkills(DataContext context){
+        if (context.Skills.Any()) return;
+       
+        var coreSkills = new List<Skill>
+        {
+          new Skill
+          {
+            Title = "Listening"
+          },
+          new Skill
+          {
+            Title = "Reading"
+          },
+          new Skill
+          {
+            Title = "Speaking"
+          },
+          new Skill
+          {
+            Title = "Writing"
+          },
+          new Skill
+          {
+            Title = "Grammar"
+          },
+        };
 
-            
-              var coreSkills = new List<Skill>
-              {
-                new Skill
-                {
-                  Title = "Listening"
-                },
-                new Skill
-                {
-                  Title = "Reading"
-                },
-                new Skill
-                {
-                  Title = "Speaking"
-                },
-                new Skill
-                {
-                  Title = "Writing"
-                },
-                new Skill
-                {
-                  Title = "Grammar"
-                },
-              };
+        await context.Skills.AddRangeAsync(coreSkills);            
+        await context.SaveChangesAsync();
 
-              await context.Skills.AddRangeAsync(coreSkills);
-            
-            }
-            await context.SaveChangesAsync();
+
+      }
+      public static async Task SeedData(DataContext context)
+        {
+
+          await SeedLanguages(context);
+          await SeedSkills(context);
+
         }
     }
 }
