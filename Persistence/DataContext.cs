f07e976df475d52d6c475b47c13d30e339c21849
Dominique <carinefrvga@gmail.com>
2023-07-27 16:26:41 -0300

    fix: relationship in datacontext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -15,35 +15,39 @@ namespace Persistence
     public DbSet<Language> Languages { get; set; }
     public DbSet<Skill> Skills { get; set; }
     public DbSet<Vocabulary> Vocabularies { get; set; }
-
     public DbSet<VocabularyList> VocabularyLists { get; set; }
-
-    // public DbSet<TagForVocab> TagsForVocab { get; set; }
+    public DbSet<Log> Logs { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       base.OnModelCreating(modelBuilder);
 
-      // modelBuilder.Entity<VocabularyTag>(x => x.HasKey(vt => new { vt.VocabularyId, vt.TagId }));
-
-      // modelBuilder.Entity<VocabularyTag>()
-      //   .HasOne(vt => vt.Vocabulary)
-      //   .WithMany(vt => vt.Tags)
-      //   .HasForeignKey(vt => vt.VocabularyId); // fk of this table that points to Vocab
-
-      // modelBuilder.Entity<VocabularyTag>()
-      //   .HasOne(vt => vt.Tag)
-      //   .WithMany(vt => vt.Vocabularies)
-      //   .HasForeignKey(vt => vt.TagId);
-      // modelBuilder.Entity<VocabularyList>().HasMany(vl=> Vocabularies).WithOne(v=>v.VocabularyList);
-
-      // modelBuilder.Entity<
-
       modelBuilder.Entity<LearningSpace>()
         .HasOne(b => b.Language)
         .WithMany(a => a.LearningSpaces)
         .HasForeignKey(b => b.LanguageId);
+
+      modelBuilder.Entity<VocabularyList>()
+        .HasOne(v=>v.LearningSpace)
+        .WithMany(l=>l.VocabularyLists)
+        .HasForeignKey(v=>v.LearningSpaceId);
+
+      modelBuilder.Entity<Vocabulary>()
+        .HasOne(v=>v.VocabularyList)
+        .WithMany(vl=>vl.Vocabularies)
+        .HasForeignKey(v=>v.VocabularyListId);
+
+      modelBuilder.Entity<Log>()
+        .HasOne(l=>l.LearningSpace)
+        .WithMany(ls=>ls.Logs)
+        .HasForeignKey(l=>l.LearningSpaceId);
+
+      modelBuilder.Entity<Log>()
+        .HasOne(l=>l.SkillDedicated)
+        .WithMany(s=>s.Logs)
+        .HasForeignKey(l=>l.SkillId);
+
     }
   }
 }
