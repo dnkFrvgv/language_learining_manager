7e61da99c15e062daaa88e0132079d0aa4f2674b
Dominique <carinefrvga@gmail.com>
2023-07-28 13:15:49 -0300

    vocabulary field name change

--- Domain/Entities/Vocabulary.cs
+++ Domain/Entities/Vocabulary.cs
@@ -3,16 +3,18 @@ namespace Domain.Entities
   public class Vocabulary : BaseEntity
   {
     public string Title { get; set; }
-    public string ExamplePhrase { get; set; }
+    public string ContextPhrase { get; set; }
     public string Translation { get; set; }
-    public DateTime Date { get; set; }
+    public String Source { get; set; }
+    public DateTime DateAdded { get; set; }
     public StatusVocab Status { get; set; } = StatusVocab.NEW;
     public Guid VocabularyListId { get; set; }
     public VocabularyList VocabularyList { get; set; }
   }
 
   public enum StatusVocab {
-    MEMORIZED = 1,
+    ACTIVE = 1,
     NEW = 2,
+    PASSIVE = 3
   }
 }
---
+++ Persistence/Migrations/20230728125218_ChangeVocabFieldsName.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeVocabFieldsName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vocabularies_VocabularyLists_VocabularyListId",
+                table: "Vocabularies");
+
+            migrationBuilder.RenameColumn(
+                name: "ExamplePhrase",
+                table: "Vocabularies",
+                newName: "Source");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Vocabularies",
+                newName: "DateAdded");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "VocabularyListId",
+                table: "Vocabularies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContextPhrase",
+                table: "Vocabularies",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vocabularies_VocabularyLists_VocabularyListId",
+                table: "Vocabularies",
+                column: "VocabularyListId",
+                principalTable: "VocabularyLists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vocabularies_VocabularyLists_VocabularyListId",
+                table: "Vocabularies");
+
+            migrationBuilder.DropColumn(
+                name: "ContextPhrase",
+                table: "Vocabularies");
+
+            migrationBuilder.RenameColumn(
+                name: "Source",
+                table: "Vocabularies",
+                newName: "ExamplePhrase");
+
+            migrationBuilder.RenameColumn(
+                name: "DateAdded",
+                table: "Vocabularies",
+                newName: "Date");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "VocabularyListId",
+                table: "Vocabularies",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vocabularies_VocabularyLists_VocabularyListId",
+                table: "Vocabularies",
+                column: "VocabularyListId",
+                principalTable: "VocabularyLists",
+                principalColumn: "Id");
+        }
+    }
+}
